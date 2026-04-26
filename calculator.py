460f656a171fb820e1f41b2a2130f7903c5d7123
Domigg23 <domigg23@example.com>
2026-04-26 15:30:51 +0200

    Update main block with all functions

--- calculator.py
+++ calculator.py
@@ -1,10 +1,14 @@
+
 def add(a, b):
     return a + b
 
-if __name__ == "__main__":
-    print("5 + 3 =", add(5, 3))
 def subtract(a, b):
     return a - b
 
 def multiply(a, b):
     return a * b
+
+if __name__ == "__main__":
+    print("5 + 3 =", add(5, 3))
+    print("5 - 3 =", subtract(5, 3))
+    print("5 * 3 =", multiply(5, 3))
